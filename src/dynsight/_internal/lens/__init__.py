7175923fc9fba5d2812dec6f764dc50bfe030d9f
andrewtarzia <andrew.tarzia@gmail.com>
2023-11-16 20:28:55 +0100

    Add skeleton modules, tests.

---
+++ src/dynsight/_internal/lens/__init__.py
@@ -0,0 +1,10 @@
+def foo2(a: int) -> str:
+    """Do stuff.
+
+    Parameters:
+        a: A number.
+
+    Returns:
+        A message for you.
+    """
+    return f"Hello, World! Also: {a}"
---
+++ src/dynsight/hdf5er.py
@@ -0,0 +1,6 @@
+"""hdf5er package."""
+from dynsight._internal.hdf5er import foo1
+
+__all__ = [
+    "foo1",
+]
