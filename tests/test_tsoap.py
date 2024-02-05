ce193aed467dca7d98c6f8cf18b3831176160bc6
SimoneMartino98 <s.martino0898@gmail.com>
2024-02-05 14:37:51 +0100

    test_soap

--- tests/test_tsoap.py
+++ tests/test_tsoap.py
@@ -54,10 +54,10 @@ def test_time_soap_vectors() -> None:
 
             # check if control and test array are equal
             assert np.allclose(
-                timed_soap, check_timed_soap, atol=1e-15, rtol=1e-15
+                timed_soap, check_timed_soap, atol=1e-12, rtol=1e-12
             )
             assert np.allclose(
-                delta_time_soap, check_delta_time_soap, atol=1e-15, rtol=1e-15
+                delta_time_soap, check_delta_time_soap, atol=1e-12, rtol=1e-12
             )
     # if test passed remove test_soap array from test folder
     Path(output_file).unlink()
