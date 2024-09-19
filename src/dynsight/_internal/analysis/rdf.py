c8c255efc0aa5a13ea697298ee21146c7fee0094
SimoneMartino98 <s.martino0898@gmail.com>
2024-09-19 16:22:23 +0200

    added docs

--- src/dynsight/_internal/analysis/rdf.py
+++ src/dynsight/_internal/analysis/rdf.py
@@ -18,7 +18,18 @@ class RDF:
     """Object for computing and analyzing the Radial Distribution Function.
 
     The Radial Distribution Function (RDF) describes how particle density
-    varies as a function of the distance from a reference particle.
+    varies as a function of the distance.
+
+    Attributes:
+        trajectory (list[Path]): List of file paths including trajectory
+        and optional topology file.
+        pipeline (Pipeline): OVITO pipeline used for importing and modifying
+        the trajectory data.
+        pair_distances (np.ndarray): Array of distances between particle pairs.
+        rdf (np.ndarray): Array of RDF values corresponding to
+        the pair distances.
+        minima_points (tuple): Tuple containing arrays of minima point
+        distances and RDF values.
     """
 
     def __init__(
@@ -27,6 +38,16 @@ class RDF:
         topology_file: Path | None = None,
         xyz_cols: list[str] | None = None,
     ) -> None:
+        """Initialize the RDF object and import the trajectory data.
+
+        Args:
+            trajectory_file (Path): Path to the trajectory
+            file (e.g. `.xyz` or `.xtc`).
+            topology_file (Path | None, optional): Path to the topology file
+            if required (e.g. `.gro`).
+            xyz_cols (list[str] | None, optional): List of column names for
+            XYZ format files (e.g., `["Particle Type", "X", "Y", "Z"]`).
+        """
         # Check number of args and setup the trajectory files
         if topology_file is None:
             self.trajectory = [Path(trajectory_file)]
@@ -45,10 +66,37 @@ class RDF:
     def compute_rdf(
         self,
         cutoff: float,
-        bins: int,
+        bins: int = 200,
         remove_atoms: str | None = None,
         step: int = 1,
     ) -> None:
+        """Compute the Radial Distribution Function (RDF).
+
+        Args:
+            cutoff (float): Maximum distance for calculating the RDF.
+            bins (int): Number of bins for the RDF calculation, Default is 200.
+            remove_atoms (str | None, optional): Selection OVITO expression
+            for atoms to be removed (e.g., 'ParticleType == 1').
+            step (int, optional): Frequency of time sampling for averaging
+            the RDF. Default is 1.
+
+        Important:
+            If you're using the `remove_atoms` argument to remove certain
+            particles from the RDF calculation, you need to use the
+            OVITO's expression syntax for selecting particles.
+
+            Example expressions:
+                - `"ParticleType == 1"`: Selects all particles of type 1.
+                - `"Position.X > 0"`: Selects particles with an x-coordinate
+                greater than zero.
+                - `"ParticleIdentifier % 2 == 0"`: Selects particles with even
+                particle identifiers.
+            Be sure to have this type of information stored in your
+            simulation file before using this command.
+
+            More information on OVITO's expression syntax can be found here:
+            https://www.ovito.org/manual/reference/pipelines/modifiers/expression_select.html
+        """
         # Delete selected atoms (OVITO commands)
         if remove_atoms is not None:
             selection_modifier = ExpressionSelectionModifier(
@@ -76,20 +124,23 @@ class RDF:
         self.rdf = total_rdf[:, 1]
 
     def find_minima_points(self, prominence: float) -> None:
+        """Find local minima in the RDF.
+
+        Args:
+            prominence (float): Minimum prominence of peaks in the RDF
+            for identifying minima points. Tune this parameter to obtain
+            better performance.
+        """
         inverted_rdf = -self.rdf
         peaks, properties = find_peaks(inverted_rdf, prominence=prominence)
         self.minima_points = [self.pair_distances[peaks], self.rdf[peaks]]
 
-    def rdf_plot(self, minpoints: bool) -> None:
+    def rdf_plot(self, minpoints: bool = False) -> None:
         """Plot the RDF curve with optional marking of the minimum points.
 
         Args:
-            minpoints (bool): If True, the local minimum points are
-            highlighted on the RDF plot.
-
-        Raises:
-            ValueError: If RDF has not been computed yet or if minimum points
-            have not been computed when requested.
+            minpoints (bool, optional): If `True`, the local minimum points
+            are highlighted on the RDF plot. Default is `False`.
         """
         plt.plot(self.pair_distances, self.rdf, label="RDF", color="black")
         if minpoints:
