bcab3d88c2665474e3a80f1af937382bf291c198
SimoneMartino98 <s.martino0898@gmail.com>
2024-09-19 10:20:54 +0200

    added docs and __init__ file

--- src/dynsight/_internal/analysis/rdf.py
+++ src/dynsight/_internal/analysis/rdf.py
@@ -16,21 +16,55 @@ from scipy.signal import argrelextrema
 
 
 class RDF:
-    """Radial Distribution Function."""
+    """Object for computing and analyzing the Radial Distribution Function.
+
+    The Radial Distribution Function (RDF) describes how particle density
+    varies as a function of the distance from a reference particle.
+    """
 
     def __init__(self) -> None:
-        """Initialization funciton for RDF object."""
+        """Initialization function for RDF object.
+
+        Attributes:
+            pipeline (ovito.pipeline.Pipeline | None): OVITO pipeline object
+            that handles data import and processing.
+            min_points (np.ndarray | None): Array storing the indices of
+            the local minima in the RDF data.
+            rdf_bins (np.ndarray | None): Array of bin centers corresponding
+            to the RDF data.
+            rdf (np.ndarray | None): Computed RDF values.
+        """
         self.pipeline = None
         self.min_points = None
         self.rdf_bins = None
         self.rdf = None
 
     def read_from_xyz(self, input_file: Path, columns: tuple[str]) -> None:
-        """Read trajectory from xyz file."""
+        """Read trajectory data from an XYZ file.
+
+        Args:
+            input_file (Path): Path to the input XYZ file containing
+            atomic positions.
+            columns (tuple[str]): Column names for XYZ data to map to the
+            correct attributes.
+
+        Raises:
+            FileNotFoundError: If the specified file does not exist.
+        """
         self.pipeline = import_file(Path(input_file), columns=columns)
 
     def read_from_gromacs(self, topo_file: Path, traj_file: Path) -> None:
-        """Read trajectory from GROMACS .gro and .xtc files."""
+        """Read trajectory from GROMACS .gro and .xtc files.
+
+        Args:
+            topo_file (Path): Path to the topology file (.gro) used for
+            GROMACS simulations.
+            traj_file (Path): Path to the trajectory file (.xtc) used for
+            GROMACS simulations.
+
+        Raises:
+            FileNotFoundError: If the specified files do not exist.
+        """
         self.pipeline = import_file(Path(topo_file))
         trajectory = LoadTrajectoryModifier()
         trajectory.source.load(Path(traj_file))
@@ -43,7 +77,21 @@ class RDF:
         remove_atoms: str | None = None,
         step: int = 1,
     ) -> np.ndarray:
-        """RDF computation."""
+        """Compute the RDF using the loaded trajectory.
+
+        Args:
+            cutoff (float): The maximum distance (cutoff) for calculating the
+            RDF.
+            bins (int): Number of bins to divide the radial distances into.
+            remove_atoms (str | None, optional): A selection
+            string (OVITO expression language) to define which atoms should
+            be removed before RDF calculation.
+            step (int, optional): Sampling frequency (in timesteps) for
+            time-averaging the RDF. Default is 1.
+
+        Raises:
+            ValueError: If no trajectory is loaded in the pipeline.
+        """
         if self.pipeline is None:
             raise_message = "Unloaded trajectory."
             raise ValueError(raise_message)
@@ -72,14 +120,30 @@ class RDF:
         self.rdf = total_rdf[:, 1]
 
     def find_minpoints(self) -> np.ndarray:
-        """Find minumum points in RDF."""
+        """Find local minima points in the RDF data.
+
+        This method uses scipy's `argrelextrema` function to find the
+        indices of local minima in the RDF.
+
+        Raises:
+            ValueError: If the RDF has not been computed yet.
+        """
         if self.rdf is None:
             raise_message = "RDF has not been computed yet."
             raise ValueError(raise_message)
         self.min_points = argrelextrema(self.rdf, np.less)
 
     def rdf_plot(self, minpoints: bool) -> None:
-        """Plot the RDF."""
+        """Plot the RDF curve with optional marking of the minimum points.
+
+        Args:
+            minpoints (bool): If True, the local minimum points are
+            highlighted on the RDF plot.
+
+        Raises:
+            ValueError: If RDF has not been computed yet or if minimum points
+            have not been computed when requested.
+        """
         if self.rdf is None:
             raise_message = "RDF has not been computed yet."
             raise ValueError(raise_message)
