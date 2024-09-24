38da7ed10a35dd51c1c1ad7ed058dd75809ca257
SimoneMartino98 <s.martino0898@gmail.com>
2024-09-24 18:16:23 +0200

    mode 1

--- src/dynsight/_internal/analysis/rdf.py
+++ src/dynsight/_internal/analysis/rdf.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 import matplotlib.pyplot as plt
 import os
-os.environ['OVITO_GUI_MODE'] = '0' # noqa: Q000
+os.environ['OVITO_GUI_MODE'] = '1' # noqa: Q000
 from ovito.io import import_file
 from ovito.modifiers import (
     CoordinationAnalysisModifier,
