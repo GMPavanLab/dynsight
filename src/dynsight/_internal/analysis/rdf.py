b1265551d6483dcff369c0f605c069d42b4f5e65
SimoneMartino98 <s.martino0898@gmail.com>
2024-09-30 14:01:19 +0200

    initialize the doc

--- src/dynsight/_internal/analysis/rdf.py
+++ src/dynsight/_internal/analysis/rdf.py
@@ -1,32 +1,48 @@
 from __future__ import annotations
-import MDAnalysis as mda
+
+from typing import TYPE_CHECKING, Any
+
+if TYPE_CHECKING:
+    import MDAnalysis
+    import numpy as np
+
 from MDAnalysis.analysis.rdf import InterRDF
 
+
 def compute_rdf(
-        universe: mda.Universe,
-        distances_range: list[float],
-        s1: str = "all",
-        s2: str = "all",
-        exclusion_block: list[int] = [1,1],
-        nbins: int = 200,
-        start: int = None,
-        stop: int = None,
-        step: int = 1,
-        verbose: bool = True
-        ):
+    universe: MDAnalysis.Universe,
+    distances_range: list[float],
+    s1: str = "all",
+    s2: str = "all",
+    exclusion_block: list[int] | None = None,
+    nbins: int = 200,
+    start: int | None = None,
+    stop: int | None = None,
+    step: int = 1,
+    verbose: bool = True,
+) -> tuple[np.ndarray[int, Any], np.ndarray[float, Any]]:
+    r"""Radial Distribution Function between two types of particles.
+
+    prova prova.
+
+    $$
+    g_{ab}(r) = \frac{\langle \rho_b(r) \rangle}{\rho_b}
+    $$
+    bye.
+    """
+    if exclusion_block is None:
+        exclusion_block = [1, 1]
+
     selection_1 = universe.select_atoms(s1)
     selection_2 = universe.select_atoms(s2)
+
     rdf = InterRDF(
         g1=selection_1,
         g2=selection_2,
         nbins=nbins,
         range=distances_range,
-        exclusion_block=exclusion_block
+        exclusion_block=exclusion_block,
     )
-    rdf.run(
-        verbose=verbose,
-        start=start,
-        stop=stop,
-        step=step
-    )
+
+    rdf.run(verbose=verbose, start=start, stop=stop, step=step)
     return rdf.bins, rdf.rdf
--- src/dynsight/analysis.py
+++ src/dynsight/analysis.py
@@ -1,13 +1,8 @@
 """analysis package."""
 
+from dynsight._internal.analysis.rdf import compute_rdf
 from dynsight._internal.analysis.shannon_entropy import (
     compute_entropy_gain,
 )
-from dynsight._internal.analysis.rdf import(
-    compute_rdf
-)
 
-__all__ = [
-    "compute_entropy_gain",
-    "compute_rdf"
-]
+__all__ = ["compute_entropy_gain", "compute_rdf"]
