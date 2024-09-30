4d0ed75e4b17f36ecab94104317eff3e5f05d8cf
SimoneMartino98 <s.martino0898@gmail.com>
2024-09-30 11:45:48 +0200

    rdf code plugged to dyns

--- src/dynsight/analysis.py
+++ src/dynsight/analysis.py
@@ -3,7 +3,11 @@
 from dynsight._internal.analysis.shannon_entropy import (
     compute_entropy_gain,
 )
+from dynsight._internal.analysis.rdf import(
+    compute_rdf
+)
 
 __all__ = [
     "compute_entropy_gain",
+    "compute_rdf"
 ]
