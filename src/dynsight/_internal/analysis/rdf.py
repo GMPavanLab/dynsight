cb1a6a7ec8b575ea03e0e284bb1c3c2fc159fda8
SimoneMartino98 <s.martino0898@gmail.com>
2024-09-30 16:54:23 +0200

    added docs

--- src/dynsight/_internal/analysis/rdf.py
+++ src/dynsight/_internal/analysis/rdf.py
@@ -19,22 +19,52 @@ def compute_rdf(
     start: int | None = None,
     stop: int | None = None,
     step: int = 1,
-    verbose: bool = True,
 ) -> tuple[np.ndarray[int, Any], np.ndarray[float, Any]]:
     r"""Radial Distribution Function between two types of particles.
 
-    prova prova.
+    The RDF between two types of particles `a` and `b` is:
 
-    $$
-    g_{ab}(r) = \frac{\langle \rho_b(r) \rangle}{\rho_b}
-    $$
-    bye.
+    .. math::
+        g_{ab}(r) = (N_a N_b)^{-1} \sum_{i=1}^{N_a}\sum_{j=1}^{N_b}
+        \langle \sigma(|\mathbf{r_i}\mathbf{r_j}| - r)\rangle
+
+    The radial distribution function is calculated by histogramming distances
+    between two groups of atom `s1` and `s2`. Periodic boundary conditions are
+    taken into account via the minimum-image convention.
+
+    Parameters:
+        universe:
+            Simulation MDAnalysis universe.
+        s1:
+            First atom group.
+        s2:
+            Second atom group.
+        distances_range:
+            Initial and final distance within which compute the RDF.
+        exclusion_block:
+            A tuple specifying the size of blocks
+            (e.g., molecules) to exclude distances between atoms within the
+            same block. If `s1` and `s2` are equal, it prevents
+            self-interactions by default with (1, 1).
+        nbins:
+            The number of bins used to divide the distance range for histogram
+            the RDF.
+        start:
+            Initial molecular dynamics step.
+        stop:
+            Final molecular dynamics step.
+        step:
+            Frequency at which the dynamic is sampled.
+
+    Returns:
+        Two arrays where the pair separation distance and the RDF values
+        are stored respectively.
     """
-    if exclusion_block is None:
-        exclusion_block = [1, 1]
-
     selection_1 = universe.select_atoms(s1)
     selection_2 = universe.select_atoms(s2)
+
+    if exclusion_block is None and selection_1 == selection_2:
+        exclusion_block = [1, 1]
 
     rdf = InterRDF(
         g1=selection_1,
@@ -44,5 +74,5 @@ def compute_rdf(
         exclusion_block=exclusion_block,
     )
 
-    rdf.run(verbose=verbose, start=start, stop=stop, step=step)
+    rdf.run(verbose=True, start=start, stop=stop, step=step)
     return rdf.bins, rdf.rdf
