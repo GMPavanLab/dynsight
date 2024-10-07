8b7a83ef357bd14d8628e8b1e16af3e8f469cb07
Matteo Becchi <bechmath@gmail.com>
2024-10-07 12:18:13 +0200

    Adding the module onion.helpers

--- src/dynsight/onion.py
+++ src/dynsight/onion.py
@@ -3,6 +3,7 @@
 from dynsight._internal.onion import (
     OnionMulti,
     OnionUni,
+    helpers,
     onion_multi,
     onion_uni,
     plot,
@@ -13,5 +14,6 @@ __all__ = [
     "OnionMulti",
     "onion_uni",
     "onion_multi",
+    "helpers",
     "plot",
 ]
