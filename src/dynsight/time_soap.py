380f4987c87326c4b1370c96ccd20526cc75016b
Matteo Becchi <bechmath@gmail.com>
2025-01-28 15:22:11 +0100

    New verions of SOAP and tSOAP functions

--- src/dynsight/time_soap.py
+++ src/dynsight/time_soap.py
@@ -1,13 +1,9 @@
 """time SOAP package."""
 
 from dynsight._internal.timesoap.timesoap import (
-    gettimesoapsimple,
     timesoap,
-    timesoapsimple,
 )
 
 __all__ = [
-    "gettimesoapsimple",
     "timesoap",
-    "timesoapsimple",
 ]
