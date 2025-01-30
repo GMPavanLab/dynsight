185b3f7cb795f7e2ad2c9ebc8a037b0c112ccd5e
Matteo Becchi <bechmath@gmail.com>
2025-01-30 09:12:40 +0100

    Handling missing SOAPify

--- src/dynsight/__init__.py
+++ src/dynsight/__init__.py
@@ -2,13 +2,21 @@
 
 from dynsight import (
     analysis,
-    data_processing,
-    hdf5er,
     lens,
     onion,
     soap,
     utilities,
 )
+
+try:
+    from dynsight import data_processing
+except ModuleNotFoundError:
+    data_processing = None
+
+try:
+    from dynsight import hdf5er
+except ModuleNotFoundError:
+    hdf5er = None
 
 __all__ = [
     "analysis",
