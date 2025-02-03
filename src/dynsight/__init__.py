08c50d6407e2601a1e7f7f15de6cff5c1fdbc8cb
Matteo Becchi <bechmath@gmail.com>
2025-02-03 15:31:54 +0100

    All modules back to being always there

--- src/dynsight/__init__.py
+++ src/dynsight/__init__.py
@@ -1,9 +1,9 @@
 """dynsight package."""
 
-import contextlib
-
 from dynsight import (
     analysis,
+    data_processing,
+    hdf5er,
     lens,
     onion,
     soap,
@@ -12,18 +12,10 @@ from dynsight import (
 
 __all__ = [
     "analysis",
+    "data_processing",
+    "hdf5er",
     "lens",
     "onion",
     "soap",
     "utilities",
 ]
-
-with contextlib.suppress(ModuleNotFoundError):
-    from dynsight import data_processing  # Only if cpctools is installed
-
-    __all__ += ["data_processing"]
-
-with contextlib.suppress(ModuleNotFoundError):
-    from dynsight import hdf5er  # Only if cpctools is installed
-
-    __all__ += ["hdf5er"]
