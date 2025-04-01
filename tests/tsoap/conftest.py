30279af18525cdb79ab8757907de451d08cc316e
SimoneMartino98 <s.martino0898@gmail.com>
2025-04-01 10:25:57 +0200

    minor fixes

--- tests/tsoap/conftest.py
+++ tests/tsoap/conftest.py
@@ -15,14 +15,14 @@ from tests.tsoap.case_data import TimeSOAPCaseData
         ),
         # Case 1: changing the cutoff radius
         lambda name: TimeSOAPCaseData(
-            expected_tsoap="c0_tsoap_rc4_d1.npy",
+            expected_tsoap="c1_tsoap_rc4_d1.npy",
             r_c=4,
             delay=1,
             name=name,
         ),
         # Case 2: changing the delay
         lambda name: TimeSOAPCaseData(
-            expected_tsoap="c0_tsoap_rc4_d4.npy",
+            expected_tsoap="c2_tsoap_rc4_d4.npy",
             r_c=8,
             delay=4,
             name=name,
