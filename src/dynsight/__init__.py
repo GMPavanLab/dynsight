c39504ac9ef1a07b9838f0d4d008c3ab77a7b182
Simone Martino <s.martino0898@gmail.com>
2025-04-14 17:42:33 +0200

    Initializing dynsight vision

--- src/dynsight/__init__.py
+++ src/dynsight/__init__.py
@@ -8,6 +8,7 @@ from dynsight import (
     onion,
     soap,
     utilities,
+    vision,
 )
 
 __all__ = [
@@ -18,4 +19,5 @@ __all__ = [
     "onion",
     "soap",
     "utilities",
+    "vision",
 ]
