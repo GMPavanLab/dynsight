e5a362b6297084946b866a3278c73015ee939ea5
Matteo Becchi <bechmath@gmail.com>
2025-04-23 14:30:16 +0200

    First complete setup

--- examples/analysis_workflow.py
+++ examples/analysis_workflow.py
@@ -10,20 +10,21 @@ def main() -> None:
     # Create a Trj object
     universe = MDAnalysis.Universe("trajectory.xtc", "trajectory.gro")
     water_trj = Trj(universe)
-    water_trj.save()
+    water_trj.save("water_trj")
 
     # We want for instance compute LENS on this trajectory
-    water_lens = water_trj.get_lens(r_cut=10.0)  # This is an Insight object
+    # This is an Insight object:
+    water_lens = water_trj.get_lens(r_cut=10.0)
 
     # We can do spatial average on the computed LENS
-    water_lens.spatial_average(water_trj)
+    _ = water_lens.spatial_average(water_trj, r_cut=10.0)
 
     # And we can perform onion-clustering. Here there should be some way to
     # do all the calculations in parallel using 'yield' in the method...
-    water_lens.get_onion(delta_t=10)
+    _ = water_lens.get_onion(delta_t=10)
 
     # Save the Insight with all the results
-    water_lens.save()
+    water_lens.save("water_lens")
 
 
 if __name__ == "__main__":
--- src/dynsight/_internal/trajectory/trajectory.py
+++ src/dynsight/_internal/trajectory/trajectory.py
@@ -1,42 +1,131 @@
 from __future__ import annotations
 
+import pickle
+from dataclasses import dataclass, field
+from pathlib import Path
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     import MDAnalysis
     import numpy as np
     from numpy.typing import NDArray
+    from tropea_clustering._internal.first_classes import StateUni
 
 import dynsight
 
 
+@dataclass
 class Insight:
-    def __init__(self, data: NDArray[np.float64], param: float) -> None:
-        self.dataset = data
-        self.param = param
+    """Contains an analysis perfomed on a many-body trajectory."""
 
-    def spatial_average(self, trajectory: Trj) -> None:
-        pass
+    dataset: NDArray[np.float64]
+    r_cut: float
 
-    def get_onion(self, delta_t: int) -> None:
-        pass
+    def spatial_average(
+        self,
+        trajectory: Trj,
+        r_cut: float,
+        selection: str = "all",
+        num_processes: int = 1,
+    ) -> Insight:
+        averaged_dataset = dynsight.analysis.spatialaverage(
+            universe=trajectory.universe,
+            descriptor_array=self.dataset,
+            selection=selection,
+            cutoff=r_cut,
+            num_processes=num_processes,
+        )
+        return Insight(averaged_dataset, r_cut)
 
-    def save(self) -> None:
-        pass
+    def get_onion(self, delta_t: int) -> ClusterInsight:
+        reshaped_data = dynsight.onion.helpers.reshape_from_nt(
+            self.dataset, delta_t
+        )
+        state_list, labels = dynsight.onion.onion_uni(reshaped_data)
+        return ClusterInsight(
+            self.dataset,
+            self.r_cut,
+            delta_t,
+            state_list,
+            reshaped_data,
+            labels,
+        )
+
+    def save(self, file_name: str) -> None:
+        file_path = Path(f"{file_name}.pkl")
+        with Path.open(file_path, "wb") as file:
+            pickle.dump(self, file)
 
 
+@dataclass
+class ClusterInsight(Insight):
+    """Contains a clustering analysis."""
+
+    delta_t: int
+    state_list: list[StateUni]
+    reshaped_data: NDArray[np.float64]
+    labels: NDArray[np.int64]
+
+    def plot_output(self, file_name: str) -> None:
+        dynsight.onion.plot.plot_output_uni(
+            file_name,
+            self.reshaped_data,
+            self.dataset.shape[0],
+            self.state_list,
+        )
+
+    def plot_one_trj(self, particle_id: int, file_name: str) -> None:
+        dynsight.onion.plot.plot_one_trj_uni(
+            file_name,
+            particle_id,
+            self.reshaped_data,
+            self.dataset.shape[0],
+            self.labels,
+        )
+
+    def plot_medoids(self, file_name: str) -> None:
+        dynsight.onion.plot.plot_medoids_uni(
+            file_name,
+            self.reshaped_data,
+            self.labels,
+        )
+
+    def plot_state_populations(self, file_name: str) -> None:
+        dynsight.onion.plot.plot_state_populations(
+            file_name,
+            self.dataset.shape[0],
+            self.delta_t,
+            self.labels,
+        )
+
+    def plot_sankey(self, file_name: str, frame_list: list[int]) -> None:
+        dynsight.onion.plot.plot_sankey(
+            file_name,
+            self.labels,
+            self.dataset.shape[0],
+            frame_list,
+        )
+
+
+@dataclass
 class Trj:
-    def __init__(self, universe: MDAnalysis.Universe) -> None:
-        self.trj = universe
-        self.dt = universe.trajectory.dt
+    """Contains a many-body trajectory."""
+
+    universe: MDAnalysis.Universe
+    dt: float = field(init=False)
+
+    def __post_init__(self) -> None:
+        self.dt = self.universe.trajectory.dt
 
     def get_lens(self, r_cut: float) -> Insight:
         neigcounts = dynsight.lens.list_neighbours_along_trajectory(
-            input_universe=self.trj,
+            input_universe=self.universe,
             cutoff=r_cut,
         )
         lens, *_ = dynsight.lens.neighbour_change_in_time(neigcounts)
         return Insight(lens, r_cut)
 
-    def save(self) -> None:
-        pass
+    def save(self, file_name: str) -> None:
+        file_path = Path(f"{file_name}.pkl")
+        with Path.open(file_path, "wb") as file:
+            pickle.dump(self, file)
