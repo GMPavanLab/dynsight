8b395f462e6a02370e0b6ded83fbe5b1883d8428
Matteo Becchi <bechmath@gmail.com>
2025-04-30 11:49:50 +0200

    Using JSON; frozen parameter; some changes in the classes structure

--- examples/analysis_workflow.py
+++ examples/analysis_workflow.py
@@ -4,7 +4,7 @@ from pathlib import Path
 
 import MDAnalysis
 
-from dynsight.trajectory import Trj
+from dynsight.trajectory import OnionInsight, Trj
 
 
 def main() -> None:
@@ -15,26 +15,28 @@ def main() -> None:
     universe = MDAnalysis.Universe(
         files_path / "oxygens.gro", files_path / "oxygens.xtc"
     )
-
     water_trj = Trj(universe)
-    water_trj.dump_trj(files_path / "water_trj")
 
     # We want, for instance, compute LENS on this trajectory
-    # From here, we work with Insight objects that contain data from a
-    # Trj object
+    # From here, we work with an Insight, containing data computed from a Trj
     water_lens = water_trj.get_lens(r_cut=7.5)
 
     # We can do spatial average on the computed LENS
     water_smooth = water_lens.spatial_average(water_trj, r_cut=7.5)
 
-    # Or we can perform onion-clustering
+    # And we can perform onion-clustering
     water_onion = water_smooth.get_onion(delta_t=10)
 
-    water_onion.plot_output(files_path / "tmp_fig1.png")
-    water_onion.plot_one_trj(files_path / "tmp_fig2.png", particle_id=1234)
+    water_onion.plot_output(files_path / "tmp_fig1.png", water_smooth)
+    water_onion.plot_one_trj(
+        files_path / "tmp_fig2.png",
+        water_smooth,
+        particle_id=1234,
+    )
 
-    # Save the Insight with all the results
-    water_onion.dump_insight(files_path / "water_lens")
+    # Save/load the Insight with all the results
+    water_onion.dump_to_json(files_path / "water_lens.json")
+    _ = OnionInsight.load_from_json(files_path / "water_lens.json")
 
 
 if __name__ == "__main__":
--- src/dynsight/_internal/trajectory/trajectory.py
+++ src/dynsight/_internal/trajectory/trajectory.py
@@ -1,13 +1,15 @@
 from __future__ import annotations
 
-import pickle
-from dataclasses import dataclass
-from pathlib import Path
-from typing import TYPE_CHECKING
+import json
+from dataclasses import dataclass, field, fields
+from typing import TYPE_CHECKING, Any, Self
+
+import numpy as np
 
 if TYPE_CHECKING:
+    from pathlib import Path
+
     import MDAnalysis
-    import numpy as np
     from numpy.typing import NDArray
     from tropea_clustering._internal.first_classes import StateMulti, StateUni
 
@@ -16,18 +18,35 @@ import dynsight
 UNIVAR_DIM = 2
 
 
-@dataclass
+@dataclass(frozen=True)
 class Insight:
     """Contains an analysis perfomed on a trajectory.
 
     Attributes:
         dataset: the values of a single-particle descriptor. Has shape
             (n_particles, n_frames) or (n_particles, n_frames, n_dims).
-        r_cut: the spatial cutoff with the descriptor has been computed.
     """
 
     dataset: NDArray[np.float64]
-    r_cut: float
+    meta: dict[str, Any] = field(default_factory=dict)
+
+    def dump_to_json(self, file_path: Path) -> None:
+        """Save the Insight object as .json file."""
+        data = self.__dict__
+        data["dataset"] = data["dataset"].tolist()
+        with file_path.open("w") as file:
+            json.dump(data, file, indent=4)
+
+    @classmethod
+    def load_from_json(cls, file_path: Path) -> Self:
+        """Load the Insight object from .json file."""
+        with file_path.open("r") as file:
+            data = json.load(file)
+
+        return cls(
+            dataset=np.array(data.get("dataset")),
+            meta=data.get("meta"),
+        )
 
     def spatial_average(
         self,
@@ -44,7 +63,10 @@ class Insight:
             cutoff=r_cut,
             num_processes=num_processes,
         )
-        return Insight(averaged_dataset, self.r_cut)
+        return Insight(
+            dataset=averaged_dataset,
+            meta={"sp_av_r_cut": r_cut},
+        )
 
     def get_onion(self, delta_t: int) -> OnionInsight:
         """Perform onion clustering."""
@@ -62,104 +84,156 @@ class Insight:
         onion_clust.fit(reshaped_data)
 
         return OnionInsight(
-            dataset=self.dataset,
-            r_cut=self.r_cut,
             labels=onion_clust.labels_,
-            delta_t=delta_t,
             state_list=onion_clust.state_list_,
             reshaped_data=reshaped_data,
+            meta={"delta_t": delta_t},
         )
 
-    def dump_insight(self, file_name: Path) -> None:
-        """Save a copy of the Insight object."""
-        file_path = file_name.with_suffix(".pkl")
-        with Path.open(file_path, "wb") as file:
-            pickle.dump(self, file)
 
-
-@dataclass
-class ClusterInsight(Insight):
+@dataclass(frozen=True)
+class ClusterInsight:
     """Contains a clustering analysis."""
 
     labels: NDArray[np.int64]
 
+    def dump_to_json(self, file_path: Path) -> None:
+        """Save the ClusterInsight object as .json file."""
+        data = self.__dict__
+        data["labels"] = data["labels"].tolist()
+        with file_path.open("w") as file:
+            json.dump(data, file, indent=4)
 
-@dataclass
+    @classmethod
+    def load_from_json(cls, file_path: Path) -> Self:
+        """Load the ClusterInsight object from .json file."""
+        with file_path.open("r") as file:
+            data = json.load(file)
+        return cls(labels=np.array(data.get("labels")))
+
+
+@dataclass(frozen=True)
 class OnionInsight(ClusterInsight):
     """Contains a onion-clustering analysis."""
 
-    delta_t: int
     state_list: list[StateUni] | list[StateMulti]
     reshaped_data: NDArray[np.float64]
+    meta: dict[str, Any] = field(default_factory=dict)
 
-    def plot_output(self, file_name: Path) -> None:
+    def dump_to_json(self, file_path: Path) -> None:
+        """Save the OnionInsight object as .json file."""
+        data = self.__dict__
+        data["labels"] = data["labels"].tolist()
+        new_state_list = []
+        for state in data["state_list"]:
+            tmp = {}
+            for f in fields(state):
+                value = getattr(state, f.name)
+                if isinstance(value, np.ndarray):
+                    tmp[f.name] = value.tolist()
+                else:
+                    tmp[f.name] = value
+            new_state_list.append(tmp)
+        data["state_list"] = new_state_list
+        data["reshaped_data"] = data["reshaped_data"].tolist()
+        with file_path.open("w") as file:
+            json.dump(data, file, indent=4)
+
+    @classmethod
+    def load_from_json(cls, file_path: Path) -> Self:
+        """Load the OnionInsight object from .json file."""
+        with file_path.open("r") as file:
+            data = json.load(file)
+
+        return cls(
+            labels=np.array(data.get("labels")),
+            state_list=data.get("state_list"),
+            reshaped_data=np.array(data.get("reshaped_data")),
+            meta=data.get("meta"),
+        )
+
+    def plot_output(self, file_path: Path, data_insight: Insight) -> None:
         """Plot the overall onion clustering result."""
-        if self.dataset.ndim == UNIVAR_DIM:
+        if data_insight.dataset.ndim == UNIVAR_DIM:
             dynsight.onion.plot.plot_output_uni(
-                file_name,
+                file_path,
                 self.reshaped_data,
-                self.dataset.shape[0],
+                data_insight.dataset.shape[0],
                 self.state_list,
             )
         else:
             dynsight.onion.plot.plot_output_multi(
-                file_name,
-                self.dataset,
+                file_path,
+                data_insight.dataset,
                 self.state_list,
                 self.labels,
-                self.delta_t,
+                self.meta["delta_t"],
             )
 
-    def plot_one_trj(self, file_name: Path, particle_id: int) -> None:
+    def plot_one_trj(
+        self,
+        file_path: Path,
+        data_insight: Insight,
+        particle_id: int,
+    ) -> None:
         """Plot one particle's trajectory colored according to clustering."""
-        if self.dataset.ndim == UNIVAR_DIM:
+        if data_insight.dataset.ndim == UNIVAR_DIM:
             dynsight.onion.plot.plot_one_trj_uni(
-                file_name,
+                file_path,
                 particle_id,
                 self.reshaped_data,
-                self.dataset.shape[0],
+                data_insight.dataset.shape[0],
                 self.labels,
             )
         else:
             dynsight.onion.plot.plot_one_trj_multi(
-                file_name,
+                file_path,
                 particle_id,
-                self.delta_t,
-                self.dataset,
+                self.meta["delta_t"],
+                data_insight.dataset,
                 self.labels,
             )
 
-    def plot_medoids(self, file_name: Path) -> None:
+    def plot_medoids(self, file_path: Path, data_insight: Insight) -> None:
         """Plot the average sequence of each onion cluster."""
-        if self.dataset.ndim == UNIVAR_DIM:
+        if data_insight.dataset.ndim == UNIVAR_DIM:
             dynsight.onion.plot.plot_medoids_uni(
-                file_name,
+                file_path,
                 self.reshaped_data,
                 self.labels,
             )
         else:
             dynsight.onion.plot.plot_medoids_multi(
-                file_name,
-                self.delta_t,
-                self.dataset,
+                file_path,
+                self.meta["delta_t"],
+                data_insight.dataset,
                 self.labels,
             )
 
-    def plot_state_populations(self, file_name: Path) -> None:
+    def plot_state_populations(
+        self,
+        file_path: Path,
+        data_insight: Insight,
+    ) -> None:
         """Plot each state's population along the trajectory."""
         dynsight.onion.plot.plot_state_populations(
-            file_name,
-            self.dataset.shape[0],
-            self.delta_t,
+            file_path,
+            data_insight.dataset.shape[0],
+            self.meta["delta_t"],
             self.labels,
         )
 
-    def plot_sankey(self, file_name: Path, frame_list: list[int]) -> None:
+    def plot_sankey(
+        self,
+        file_path: Path,
+        data_insight: Insight,
+        frame_list: list[int],
+    ) -> None:
         """Plot the Sankey diagram of the onion clustering."""
         dynsight.onion.plot.plot_sankey(
-            file_name,
+            file_path,
             self.labels,
-            self.dataset.shape[0],
+            data_insight.dataset.shape[0],
             frame_list,
         )
 
@@ -168,7 +242,14 @@ class OnionInsight(ClusterInsight):
 class Trj:
     """Contains a trajectory."""
 
-    universe: MDAnalysis.Universe
+    universe: MDAnalysis.Universe = field()
+
+    def init_from_universe(self, universe: MDAnalysis.Universe) -> None:
+        """Initialize Trj object from MDAnalysis.Universe.
+
+        This is equivalent to using __init()__.
+        """
+        self.universe = universe
 
     def get_lens(self, r_cut: float) -> Insight:
         """Compute LENS on the trajectory."""
@@ -177,10 +258,14 @@ class Trj:
             cutoff=r_cut,
         )
         lens, *_ = dynsight.lens.neighbour_change_in_time(neigcounts)
-        return Insight(lens, r_cut)
+        return Insight(dataset=lens, meta={"r_cut": r_cut})
 
     def get_soap(
-        self, r_cut: float, n_max: int, l_max: int, n_core: int
+        self,
+        r_cut: float,
+        n_max: int,
+        l_max: int,
+        n_core: int,
     ) -> Insight:
         """Compute SOAP on the trajectory."""
         soap = dynsight.soap.saponify_trajectory(
@@ -190,7 +275,12 @@ class Trj:
             soaplmax=l_max,
             n_core=n_core,
         )
-        return Insight(soap, r_cut)
+        attr_dict = {
+            "r_cut": r_cut,
+            "n_max": n_max,
+            "l_max": l_max,
+        }
+        return Insight(dataset=soap, meta=attr_dict)
 
     def get_timesoap(
         self, r_cut: float, n_max: int, l_max: int, n_core: int
@@ -204,10 +294,9 @@ class Trj:
             n_core=n_core,
         )
         tsoap = dynsight.soap.timesoap(soap)
-        return Insight(tsoap, r_cut)
-
-    def dump_trj(self, file_name: Path) -> None:
-        """Save a copy of the Trj object."""
-        file_path = file_name.with_suffix(".pkl")
-        with Path.open(file_path, "wb") as file:
-            pickle.dump(self, file)
+        attr_dict = {
+            "r_cut": r_cut,
+            "n_max": n_max,
+            "l_max": l_max,
+        }
+        return Insight(dataset=tsoap, meta=attr_dict)
