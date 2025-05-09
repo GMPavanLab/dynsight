dd13dff1f1b401aa786485bfa12b018c90b17623
Simone Martino <s.martino0898@gmail.com>
2025-05-09 10:09:13 +0200

    Update video_detection example with the new Vision code.

--- examples/video_detection.py
+++ examples/video_detection.py
@@ -9,18 +9,81 @@ def parse_args() -> argparse.Namespace:
     parser.add_argument(
         "--prepare",
         action="store_true",
-        help="Prepare synthesized data",
+        help="Prepare synthesized data.",
     )
     parser.add_argument(
         "--train",
         action="store_true",
-        help="Train and predict",
+        help="Train the model.",
+    )
+    parser.add_argument(
+        "--predict",
+        action="store_true",
+        help="Predict the video.",
     )
     parser.add_argument(
         "--input",
         type=Path,
         required=True,
-        help="Path to input file",
+        help="Path to input file.",
+    )
+    parser.add_argument(
+        "--output",
+        type=Path,
+        required=True,
+        help="Path to output folder.",
+    )
+    parser.add_argument(
+        "--maxcycle",
+        type=int,
+        required=False,
+        default=5,
+        help="Number of fitting cycle procedures.",
+    )
+    parser.add_argument(
+        "--epochs",
+        type=int,
+        required=False,
+        default=100,
+        help="Number of epochs for each training session.",
+    )
+    parser.add_argument(
+        "--patiece",
+        type=int,
+        required=False,
+        default=10,
+        help="Patience in terms of epochs to earlystop the procedure.",
+    )
+    parser.add_argument(
+        "--batchsize",
+        type=int,
+        required=False,
+        default=2,
+        help="BatchSize for training.",
+    )
+    parser.add_argument(
+        "--workers", type=int, required=True, help="Number of CPUs used."
+    )
+    parser.add_argument(
+        "--gpu",
+        type=tuple,
+        required=False,
+        default=None,
+        help="ID of the GPU(s) used.",
+    )
+    parser.add_argument(
+        "--step",
+        type=int,
+        required=False,
+        default=1,
+        help="Step for frame to be used.",
+    )
+    parser.add_argument(
+        "--detect_model",
+        type=Path,
+        required=False,
+        default=None,
+        help="Model to be used for predictions.",
     )
     return parser.parse_args()
 
@@ -28,19 +91,31 @@ def parse_args() -> argparse.Namespace:
 def main() -> None:
     args = parse_args()
     input_video = args.input
+    output_project = args.output
     video = dynsight.vision.Video(input_video)
-    output_folder = Path("output_folder")
-    output_folder.mkdir(exist_ok=True)
-
-    # Check if synthetic data exists if not preparing
-    if not args.prepare and not (output_folder / "synthetic_dataset").exists():
-        msg = "Synthetic dataset not found and --prepare not set."
-        raise RuntimeError(msg)
-
-    dynsight.vision.Detect(
+    detection = dynsight.vision.Detect(
         input_video=video,
-        project_folder=output_folder,
+        project_folder=output_project,
     )
+    if args.prepare:
+        detection.synthesize()
+    if args.train:
+        detection.fit(
+            initial_dataset=detection.get_project_path() / "synthetic_dataset",
+            max_sessions=args.maxcycle,
+            training_epochs=args.epochs,
+            training_patience=args.patience,
+            batch_size=args.batchsize,
+            workers=args.workers,
+            device=args.gpu,
+            frame_reading_step=args.step,
+        )
+    if args.predict:
+        detection.predict_frames(model_path=args.detect_model)
+        detection.compute_xyz(
+            detection_folder_path="prediction",
+            output_path=Path.cwd(),
+        )
 
 
 if __name__ == "__main__":
