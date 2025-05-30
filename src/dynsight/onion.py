2712f6439a1541e0810caebc36ab134dc95e8544
Matteo Becchi <bechmath@gmail.com>
2025-05-30 11:48:47 +0200

    Added functions for smooth onion clustering.

--- src/dynsight/onion.py
+++ src/dynsight/onion.py
@@ -2,18 +2,28 @@
 
 from dynsight._internal.onion import (
     OnionMulti,
+    OnionMultiSmooth,
     OnionUni,
+    OnionUniSmooth,
     helpers,
     onion_multi,
+    onion_multi_smooth,
     onion_uni,
+    onion_uni_smooth,
     plot,
+    plot_smooth,
 )
 
 __all__ = [
     "OnionMulti",
+    "OnionMultiSmooth",
     "OnionUni",
+    "OnionUniSmooth",
     "helpers",
     "onion_multi",
+    "onion_multi_smooth",
     "onion_uni",
+    "onion_uni_smooth",
     "plot",
+    "plot_smooth",
 ]
