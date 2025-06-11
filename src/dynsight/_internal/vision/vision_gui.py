210d1c6616462763a867af828446c67f49588853
Simone Martino <s.martino0898@gmail.com>
2025-06-11 14:28:58 +0200

    Commented the vision_gui code.

--- src/dynsight/_internal/vision/vision_gui.py
+++ src/dynsight/_internal/vision/vision_gui.py
@@ -10,6 +10,7 @@ from PIL import Image, ImageTk
 from PIL.Image import Resampling
 
 
+# Dataclass for storing bounding box information.
 @dataclass
 class Box:
     id: int
@@ -20,6 +21,7 @@ class Box:
     abs_coords: tuple[int, int, int, int]
 
 
+# GUI class for creating synthetic datasets from images.
 class VisionGUI:
     def __init__(
         self,
@@ -32,10 +34,12 @@ class VisionGUI:
         self.destination_folder = destination_folder
         self.master.title("Dynsight: Label tool")
 
+        # Setup the GUI window size.
         screen_width = self.master.winfo_screenwidth()
         screen_height = self.master.winfo_screenheight()
         self.master.geometry(f"{screen_width // 2}x{screen_height // 2}")
 
+        # Load the image.
         pil_image = Image.open(self.image_path)
         self.original_image = pil_image
         self.original_width, self.original_height = pil_image.size
@@ -43,13 +47,16 @@ class VisionGUI:
         self.scale = 1.0
         self.canvas_image_id: int | None = None
 
+        # GUI window layout.
         self.master.rowconfigure(index=0, weight=1)
         self.master.columnconfigure(index=0, weight=1)
         self.master.columnconfigure(index=1, weight=0)
 
+        # Canvas for image labelling.
         self.canvas = tk.Canvas(master=self.master, cursor="crosshair")
         self.canvas.grid(row=0, column=0, sticky="nsew")
 
+        # Buttons.
         self.sidebar = tk.Frame(
             master=self.master,
             width=150,
@@ -85,33 +92,37 @@ class VisionGUI:
         self.current_box = 0
         self.boxes: list[Box] = []
 
+        # Rulers.
         self.h_line = self.canvas.create_line(
-            0,
-            0,
-            0,
-            0,
+            0,  # x0
+            0,  # y0
+            0,  # x1
+            0,  # y1
             fill="blue",
             dash=(2, 2),
             width=3,
         )
         self.v_line = self.canvas.create_line(
-            0,
-            0,
-            0,
-            0,
+            0,  # x0
+            0,  # y0
+            0,  # x1
+            0,  # y1
             fill="blue",
             dash=(2, 2),
             width=3,
         )
 
+        # Bindings.
         self.canvas.bind("<Button-1>", self._on_click_press)
         self.canvas.bind("<ButtonRelease-1>", self._on_click_release)
         self.canvas.bind("<B1-Motion>", self._on_mouse_drag)
         self.canvas.bind("<Motion>", self._follow_mouse)
         self.master.bind("<Configure>", self._resize)
 
+        # Initial rendering of the image.
         self._resize()
 
+    # Resize image and redraw everything when the window is resized
     def _resize(self, _: tk.Event[Any] | None = None) -> None:
         canvas_width = self.canvas.winfo_width()
         canvas_height = self.canvas.winfo_height()
@@ -119,6 +130,7 @@ class VisionGUI:
         if canvas_width < size_thr or canvas_height < size_thr:
             return
 
+        # Scaling factor.
         self.scale = min(
             canvas_width / self.original_width,
             canvas_height / self.original_height,
@@ -130,6 +142,7 @@ class VisionGUI:
             ),
             Resampling.LANCZOS,
         )
+        # Update image and boxes.
         self.tk_image = ImageTk.PhotoImage(resized)
         self.canvas.delete("all")
         self.canvas_image_id = self.canvas.create_image(
@@ -153,35 +166,37 @@ class VisionGUI:
 
         # Recreate guide lines
         self.h_line = self.canvas.create_line(
-            0,
-            0,
-            canvas_width,
-            0,
+            0,  # x0
+            0,  # y0
+            canvas_width,  # x1
+            0,  # y1
             fill="blue",
             dash=(2, 2),
             width=3,
         )
         self.v_line = self.canvas.create_line(
-            0,
-            0,
-            0,
-            canvas_height,
+            0,  # x0
+            0,  # y0
+            0,  # x1
+            canvas_height,  # y1
             fill="blue",
             dash=(2, 2),
             width=3,
         )
 
+    # Start drawing a box.
     def _on_click_press(self, event: tk.Event[Any]) -> None:
         self.start_x, self.start_y = event.x, event.y
         self.current_box = self.canvas.create_rectangle(
-            self.start_x,
-            self.start_y,
-            self.start_x,
-            self.start_y,
+            self.start_x,  # x0
+            self.start_y,  # y0
+            self.start_x,  # x1
+            self.start_y,  # y1
             outline="red",
             width=3,
         )
 
+    # Finish drawing the box and store its data
     def _on_click_release(self, event: tk.Event[Any]) -> None:
         x2, y2 = event.x, event.y
         x1, y1 = self.start_x, self.start_y
@@ -197,6 +212,7 @@ class VisionGUI:
         width_rel = abs(x2_orig - x1_orig) / self.original_width
         height_rel = abs(y2_orig - y1_orig) / self.original_height
 
+        # Create and store Box object.
         box_info = Box(
             id=self.current_box,
             center_x=center_x,
@@ -207,37 +223,39 @@ class VisionGUI:
         )
         self.boxes.append(box_info)
         self.current_box = 0
-        self._resize()
+        self._resize()  # Redraw for consistent scaling
 
     def _on_mouse_drag(self, event: tk.Event[Any]) -> None:
         sel_x, sel_y = event.x, event.y
         self.canvas.coords(
             self.current_box,
-            self.start_x,
-            self.start_y,
-            sel_x,
-            sel_y,
+            self.start_x,  # x0
+            self.start_y,  # y0
+            sel_x,  # x1
+            sel_y,  # y1
         )
         self.canvas.coords(
             self.h_line,
-            0,
-            sel_y,
-            self.canvas.winfo_width(),
-            sel_y,
+            0,  # x0
+            sel_y,  # y0
+            self.canvas.winfo_width(),  # x1
+            sel_y,  # y1
         )
         self.canvas.coords(
             self.v_line,
-            sel_x,
-            0,
-            sel_x,
-            self.canvas.winfo_height(),
+            sel_x,  # x0
+            0,  # y0
+            sel_x,  # x1
+            self.canvas.winfo_height(),  # y1
         )
 
+    # Update box as mouse is dragged.
     def _follow_mouse(self, event: tk.Event[Any]) -> None:
         x, y = event.x, event.y
         self.canvas.coords(self.h_line, 0, y, self.canvas.winfo_width(), y)
         self.canvas.coords(self.v_line, x, 0, x, self.canvas.winfo_height())
 
+    # Save cropped images from each bounding box.
     def _submit(self) -> None:
         cropped_img_folder = self.destination_folder / "training_items"
         cropped_img_folder.mkdir(exist_ok=True)
@@ -249,11 +267,13 @@ class VisionGUI:
             cropped_image.save(save_path)
         self.master.destroy()
 
+    # Remove last drawn box.
     def _undo(self) -> None:
         if self.boxes:
             last_box = self.boxes.pop()
             self.canvas.delete(last_box.id)
             self._resize()
 
+    # Close the GUI
     def _close(self) -> None:
         self.master.destroy()
