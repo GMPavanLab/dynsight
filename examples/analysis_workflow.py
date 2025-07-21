30127abaa2262b21723fa93363f88dab6c53b6de
Matteo Becchi <bechmath@gmail.com>
2025-07-21 17:19:23 +0200

    Updated example.

--- examples/analysis_workflow.py
+++ examples/analysis_workflow.py
@@ -1,12 +1,12 @@
-"""Creating an example for the new trajectory module."""
+"""An example for the trajectory module."""
 
 from pathlib import Path
 
-from dynsight.trajectory import Insight, OnionInsight, Trj
+from dynsight.trajectory import Insight, OnionSmoothInsight, Trj
 
 
 def main() -> None:
-    """Creating an example for the new trajectory module."""
+    """An example for the trajectory module."""
     files_path = Path("analysis_workflow")
 
     # Create a Trj object
@@ -19,27 +19,25 @@ def main() -> None:
     # From here, we work with an Insight, containing data computed from a Trj
     lens_file = files_path / "lens.json"
     if lens_file.exists():
-        lens = Insight.load_from_json(lens_file)
+        lens = Insight.load_from_json(lens_file, mmap_mode="r")
     else:
         _, lens = trj.get_lens(r_cut=7.5)
         lens.dump_to_json(lens_file)
 
     # We can do spatial average on the computed LENS
     trj_lens = trj.with_slice(slice(0, -1, 1))
-    lens_smooth = lens.spatial_average(
-        trj=trj_lens, r_cut=7.5, num_processes=6
-    )
+    lens_aver = lens.spatial_average(trj=trj_lens, r_cut=7.5, num_processes=6)
 
     # And we can perform onion-clustering
-    lens_onion = lens_smooth.get_onion_smooth(delta_t=10)
+    lens_onion = lens_aver.get_onion_smooth(delta_t=10)
 
     lens_onion.plot_output(
-        file_path=files_path / "tmp_fig1.png",
-        data_insight=lens_smooth,
+        file_path=files_path / "fig1.png",
+        data_insight=lens_aver,
     )
     lens_onion.plot_one_trj(
-        file_path=files_path / "tmp_fig2.png",
-        data_insight=lens_smooth,
+        file_path=files_path / "fig2.png",
+        data_insight=lens_aver,
         particle_id=1234,
     )
     lens_onion.dump_colored_trj(
@@ -49,7 +47,9 @@ def main() -> None:
 
     # Save/load the Insight with all the results
     lens_onion.dump_to_json(files_path / "onion.json")
-    _ = OnionInsight.load_from_json(files_path / "onion.json")
+    _ = OnionSmoothInsight.load_from_json(
+        files_path / "onion.json", mmap_mode="r"
+    )
 
 
 if __name__ == "__main__":
