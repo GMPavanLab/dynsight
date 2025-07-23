fe21348319577edf6f823b9560f8e100cb98952e
Matteo Becchi <bechmath@gmail.com>
2025-07-23 10:07:12 +0200

    Psi implemented in trajectory.

--- src/dynsight/_internal/trajectory/trajectory.py
+++ src/dynsight/_internal/trajectory/trajectory.py
@@ -233,6 +233,45 @@ class Trj:
         logger.log(f"Computed SOAP with args {attr_dict}.")
         return Insight(dataset=soap, meta=attr_dict)
 
+    def get_orientational_op(
+        self,
+        r_cut: float,
+        order: int = 6,
+        selection: str = "all",
+        neigcounts: list[list[AtomGroup]] | None = None,
+    ) -> tuple[list[list[AtomGroup]], Insight]:
+        """Compute the magnitude of the orientational order parameter.
+
+        Returns:
+            neighcounts: a list[list[AtomGroup]], it can be used to speed up
+                subsequent descriptors' computations.
+            An Insight containing the orientational order parameter. It has
+                the following meta: r_cut, order, selection.
+        """
+        if neigcounts is None:
+            neigcounts = dynsight.lens.list_neighbours_along_trajectory(
+                input_universe=self.universe,
+                cutoff=r_cut,
+                selection=selection,
+                trajslice=self.trajslice,
+            )
+        psi = dynsight.descriptors.orientational_order_param(
+            self.universe,
+            neigh_list_per_frame=neigcounts,
+            order=order,
+        )
+
+        attr_dict = {"r_cut": r_cut, "order": order, "selection": selection}
+
+        logger.log(
+            f"Computed orientational order parameter using args {attr_dict}."
+        )
+
+        return neigcounts, Insight(
+            dataset=psi,
+            meta=attr_dict,
+        )
+
     def get_rdf(
         self,
         distances_range: list[float],
