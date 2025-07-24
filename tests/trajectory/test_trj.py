293e26280723078a91ccbb35d776a6bcd50a875b
Matteo Becchi <bechmath@gmail.com>
2025-07-24 16:08:29 +0200

    Debugging.

--- tests/trajectory/test_trj.py
+++ tests/trajectory/test_trj.py
@@ -96,7 +96,12 @@ def test_get_descriptors(file_paths: dict[str, Path]) -> None:
     # psi and tsoap fail pytest for no apparent reason
     _, psi = trj.get_orientational_op(r_cut=r_cut, neigcounts=neigcounts)
     test_psi = Insight.load_from_json(file_paths["files_dir"] / "psi.json")
-    assert_allclose(test_psi.dataset, psi.dataset)
+    print(
+        np.where(
+            ~np.isclose(test_psi.dataset, psi.dataset, rtol=1e-6, atol=1e-8)
+        )
+    )
+    assert_allclose(test_psi.dataset, psi.dataset, rtol=1e-6, atol=1e-8)
 
     _ = soap.get_angular_velocity()
 
