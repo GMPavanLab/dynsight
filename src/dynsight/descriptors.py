475ce07fd23b883fa2625474a1550c6819e6e895
Matteo Becchi <bechmath@gmail.com>
2025-11-25 10:25:18 +0100

    compute_aver_align() made public.

--- src/dynsight/descriptors.py
+++ src/dynsight/descriptors.py
@@ -1,6 +1,7 @@
 """descriptors package."""
 
 from dynsight._internal.descriptors.misc import (
+    compute_aver_align,
     orientational_order_param,
     velocity_alignment,
 )
@@ -9,6 +10,7 @@ from dynsight._internal.descriptors.tica import (
 )
 
 __all__ = [
+    "compute_aver_align",
     "many_body_tica",
     "orientational_order_param",
     "velocity_alignment",
