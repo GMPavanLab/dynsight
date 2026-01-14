6cf5161873cf44fa91f7723adaff4f4a3c0ddc8e
ChiaraLionello <chia.lionello@gmail.com>
2026-01-14 09:46:49 +0000

    cleaning_cluster_population now cleans also 3D labels.

--- src/dynsight/_internal/data_processing/clusters.py
+++ src/dynsight/_internal/data_processing/clusters.py
@@ -14,18 +14,33 @@ def cleaning_cluster_population(
     assigned_env: int = 1,
 ) -> NDArray[np.int64]:
     dimension = 2
-    if labels.ndim != dimension:
+    if labels.ndim < dimension or labels.ndim > dimension + 1:
         msg = "Labels must be 2D [part,frames]"
         raise ValueError(msg)
 
-    flat = labels.ravel()
-    unique, counts = np.unique(flat, return_counts=True)
+    if labels.ndim == dimension:
+        flat = labels.ravel()
+        unique, counts = np.unique(flat, return_counts=True)
 
-    populations = counts / flat.size
-    small_clusters = unique[populations <= threshold]
+        populations = counts / flat.size
+        small_clusters = unique[populations <= threshold]
 
-    new_labels = labels.copy()
-    if small_clusters.size > 0:
-        new_labels[np.isin(labels, small_clusters)] = assigned_env
+        new_labels = labels.copy()
+        if small_clusters.size > 0:
+            new_labels[np.isin(labels, small_clusters)] = assigned_env
+
+    if labels.ndim == dimension + 1:
+        new_labels = labels.copy()
+        for i in range(labels.shape[2]):
+            lab = labels[:, :, i]
+            flat = lab.ravel()
+            unique, counts = np.unique(flat, return_counts=True)
+
+            populations = counts / flat.size
+            small_clusters = unique[populations <= threshold]
+
+            if small_clusters.size > 0:
+                mask = np.isin(lab, small_clusters)
+                new_labels[:, :, i][mask] = assigned_env
 
     return new_labels
